6db8e95ef9daf247a30dc837613a1ae938289db7
Ryzen WS <mg.coffee.man.goa@gmail.com>
2023-03-06 12:11:05 +0100

    More remaps + doc + harpoon to 1234

--- lua/mg/remap.lua
+++ lua/mg/remap.lua
@@ -1,29 +1,54 @@
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.keymap.set("v", "<leader>fs", vim.cmd.update)
-
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex, { desc = '[P]roject [v]iew (show current folder files)' })
 vim.keymap.set("n", "<leader>fs", function()
     vim.cmd('update')
     vim.lsp.buf.format()
-end, { silent = true })
+end, { silent = true, desc = '[F]ile [s]ave (saves and runs lsp format)' })
 
 -- window jumping remap
-vim.keymap.set("n", "<leader>wd", vim.cmd.close, { noremap = true })
-vim.keymap.set("n", "<leader>ww", "<C-w>w", { noremap = true })
-vim.keymap.set("n", "<leader>wj", "<C-w>j", { noremap = true })
-vim.keymap.set("n", "<leader>wk", "<C-w>k", { noremap = true })
-vim.keymap.set("n", "<leader>wl", "<C-w>l", { noremap = true })
-vim.keymap.set("n", "<leader>wm", "<C-w>m", { noremap = true })
-vim.keymap.set("n", "<leader>w/", vim.cmd.vs, { noremap = true })
+vim.keymap.set("n", "<leader>wd", vim.cmd.close, { noremap = true, desc = '[W]indow [d]elete - closes the window' })
+vim.keymap.set("n", "<leader>ww", "<C-w>w", { noremap = true, desc = 'Jumps from one [w]indow to the next [w]indow' })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { noremap = true, desc = "Jump [W]indow below" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { noremap = true, desc = "Jump [W]indow above" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { noremap = true, desc = "Jump [W]indow right" })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { noremap = true, desc = "Jump [W]indow left" })
+vim.keymap.set("n", "<leader>w/", vim.cmd.vs, { noremap = true, desc = "Split window to the right" })
 
--- terminal usage
+-- terminal
 vim.keymap.set("n", "<leader>tt", function()
     -- this doesn't open the same buffer, i have to do this via session management
     vim.cmd("sp term://sh")
-end)
-vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true })
+end, { desc = 'Opens a new terminal horizontally' })
+vim.keymap.set("t", "<Esc>", "<C-\\><C-n>", { noremap = true, desc = 'Exits terminal insert mode' })
 
--- these don't work
-vim.keymap.set("n", "<leader>w1", "<C-w>1", { noremap = true })
-vim.keymap.set("n", "<leader>w2", "<C-w>2", { noremap = true })
-vim.keymap.set("n", "<leader>w3", "<C-w>3", { noremap = true })
-vim.keymap.set("n", "<leader>w4", "<C-w>4", { noremap = true })
+-- editing
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv", { desc = 'Moves the current selection one line up' })
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv", { desc = 'Moves the current selection one line down' })
+vim.keymap.set("n", "J", "mzJ`z", { desc = 'Moves lower line to this one with cursor in place' })
+vim.keymap.set("n", "<leader>r", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left><Left>",
+    { desc = 'Search [r]eplace all instance of word under cursor' })
+
+-- navigation / NOT SURE IF I LIKE THOSE, this makes me a bit dizzy i think
+--vim.keymap.set("n", "<C-d>", "<C-d>zz", { desc = 'Half window [d]own with staying in the middle' })
+--vim.keymap.set("n", "<C-u>", "<C-u>zz", { desc = 'Half window [u]p with staying in the middle' })
+--vim.keymap.set("n", "n", "nzzzv", { desc = 'Stay in middle during [n]ext text search result' })
+--vim.keymap.set("n", "N", "Nzzzv", { desc = 'Stay in middle during Previous text search result' })
+--
+
+-- yanking to clipboard
+-- TODO: reset the clipboard usage for pasting
+vim.keymap.set("n", "<leader>y", "\"+y", { desc = "Yank to clipboard" })
+vim.keymap.set("v", "<leader>y", "\"+y", { desc = "Yank to clipboard" })
+vim.keymap.set("n", "<leader>Y", "\"+Y", { desc = "Yank to clipboard" })
+vim.keymap.set("n", "<leader>d", "\"_d", { desc = "Delete to void" })
+vim.keymap.set("v", "<leader>d", "\"_d", { desc = "Delete to void" })
+
+vim.keymap.set("n", "Q", "<nop>")
+
+--quickfix
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>", { desc = "Quickfix next" })
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>", { desc = "Quickfix previous" })
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>", { desc = "Quickfix next within project?" })
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>", { desc = "Quickfix previous within project?" })
+
+-- prep for future use
+--vim.keymap.set("n","<leader>ns","<cmd>silent !tmix neww tmux-sessionizer<CR>", { desc = "New tmux session" })
